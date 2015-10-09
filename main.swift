a5b9bc23af7598756640a79c88e49fb89b3dd63a
Yuyang Fan <yf8@uw.edu>
2015-10-08 17:38:33 -0700

    adding main.swift

--- main.swift
+++ main.swift
@@ -7,3 +7,91 @@
 //
 
 import Foundation
+
+// Standard input reading
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let result = NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
+    
+    return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+}
+
+
+// Converts given String to Double and returns result
+func convertToDouble(incoming:String) -> Double {
+    return (incoming as NSString).doubleValue
+}
+
+// Returns the factorial of a given number, returns 1 if given number is 0
+func factorial(incoming:Double) -> Double {
+    if (incoming == 0) {
+        return 1
+    } else {
+        return incoming * factorial(incoming - 1)
+    }
+}
+
+
+
+print("Enter an expression separated by returns:")
+    
+    let firstInput = input().componentsSeparatedByString(" ")
+    
+    let firstNumber = convertToDouble(firstInput[0])
+    
+    let operation = input()
+    
+
+    
+
+// Runs simple operations
+func simpleOp(firstNumber:Double, operation:String) -> Double {
+    var res: Double = 0
+    
+    switch operation {
+    case "+":
+        let secondNumber = convertToDouble(input())
+        res = firstNumber + secondNumber
+        
+    case "-":
+        let secondNumber = convertToDouble(input())
+        res = firstNumber - secondNumber
+        
+    case "*":
+        let secondNumber = convertToDouble(input())
+        res = firstNumber * secondNumber
+        
+    case "/":
+        let secondNumber = convertToDouble(input())
+        res = firstNumber / secondNumber
+        
+    case "%":
+        let secondNumber = convertToDouble(input())
+        res = firstNumber % secondNumber
+    
+    case "fact":
+         return factorial(firstNumber)
+        
+    case "count":
+        return Double(firstInput.count)
+        
+    case "avg":
+        var total: Double = 0
+        for var i = 0; i < firstInput.count; i++ {
+            total += convertToDouble(firstInput[i])
+        }
+        return (total/Double(firstInput.count))
+        
+    default:
+        res = 0
+        
+    }
+    return res
+    
+}
+
+// print stuff
+print("Result: \(simpleOp(firstNumber, operation: operation))")
+
+
